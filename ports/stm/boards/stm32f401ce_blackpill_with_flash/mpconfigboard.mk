319c47111da2ec027ba6918b2af931a661d5c1ff
evildave666 <vancleef@gmail.com>
2021-08-06 16:33:50 +0900

    Add board definitions for stm32f401ceu6 black pill and external flash version

---
+++ ports/stm/boards/stm32f401ce_blackpill_with_flash/mpconfigboard.mk
@@ -0,0 +1,18 @@
+USB_VID = 0x239A
+USB_PID = 0x006A
+USB_PRODUCT = "stm32f401ce blackpill with flash"
+USB_MANUFACTURER = "WeAct"
+
+SPI_FLASH_FILESYSTEM = 1
+#See supervisor/shared/external_flash/devices.h for options
+EXTERNAL_FLASH_DEVICES = GD25Q16C,W25Q64FV,W25Q32JVxQ,W25Q64JVxQ
+LONGINT_IMPL = MPZ
+
+INTERNAL_FLASH_FILESYSTEM = 0
+
+MCU_SERIES = F4
+MCU_VARIANT = STM32F401xE
+MCU_PACKAGE = UFQFPN48
+
+LD_COMMON = boards/common_default.ld
+LD_FILE = boards/STM32F401xe_nofs.ld
